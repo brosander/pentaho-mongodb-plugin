136189f96556f78bbe9a125dfae915c687764abd
Bryan Rosander <brosander@pentaho.com>
2014-08-22 09:40:25 -0400

    [BACKLOG-577] - Only setting mongo properties if value isn't null

--- src/main/java/org/pentaho/mongo/wrapper/MongoWrapperUtil.java
+++ src/main/java/org/pentaho/mongo/wrapper/MongoWrapperUtil.java
@@ -6,38 +6,64 @@ import org.pentaho.di.trans.steps.mongodb.MongoDbMeta;
 import org.pentaho.mongo.MongoDbException;
 import org.pentaho.mongo.MongoProp;
 import org.pentaho.mongo.MongoProperties;
+import org.pentaho.mongo.MongoUtilLogger;
 
 /**
  * Created by bryan on 8/7/14.
  */
 public class MongoWrapperUtil {
-    public static MongoClientWrapper createMongoClientWrapper(MongoDbMeta mongoDbMeta, VariableSpace vars, LogChannelInterface log) throws MongoDbException {
-        MongoProperties.Builder propertiesBuilder = new MongoProperties.Builder();
-        propertiesBuilder.set(MongoProp.HOST, vars.environmentSubstitute(mongoDbMeta.getHostnames()));
-        propertiesBuilder.set(MongoProp.PORT, vars.environmentSubstitute(mongoDbMeta.getPort()));
-        propertiesBuilder.set(MongoProp.DBNAME, vars.environmentSubstitute(mongoDbMeta.getDbName()));
-        propertiesBuilder.set(MongoProp.connectTimeout, mongoDbMeta.getConnectTimeout());
-        propertiesBuilder.set(MongoProp.socketTimeout, mongoDbMeta.getSocketTimeout());
-        propertiesBuilder.set(MongoProp.readPreference, mongoDbMeta.getReadPreference());
-        propertiesBuilder.set(MongoProp.writeConcern, mongoDbMeta.getWriteConcern());
-        propertiesBuilder.set(MongoProp.wTimeout, mongoDbMeta.getWTimeout());
-        propertiesBuilder.set(MongoProp.JOURNALED, Boolean.toString(mongoDbMeta.getJournal()));
-        propertiesBuilder.set(MongoProp.USE_ALL_REPLICA_SET_MEMBERS, Boolean.toString(mongoDbMeta.getUseAllReplicaSetMembers()));
-        propertiesBuilder.set(MongoProp.USERNAME, mongoDbMeta.getAuthenticationUser());
-        propertiesBuilder.set(MongoProp.PASSWORD, mongoDbMeta.getAuthenticationPassword());
-        propertiesBuilder.set(MongoProp.USE_KERBEROS, Boolean.toString(mongoDbMeta.getUseKerberosAuthentication()));
-        if(mongoDbMeta.getReadPrefTagSets() != null) {
-            StringBuilder tagSet = new StringBuilder();
-            for (String tag : mongoDbMeta.getReadPrefTagSets()) {
-                tagSet.append(tag);
-                tagSet.append(",");
-            }
-            // Remove trailing comma
-            if (tagSet.length() > 0) {
-                tagSet.setLength(tagSet.length() - 1);
-            }
-            propertiesBuilder.set(MongoProp.tagSet, tagSet.toString());
-        }
-        return MongoClientWrapperFactory.createMongoClientWrapper(propertiesBuilder.build(), new KettleMongoUtilLogger(log));
+  private static MongoWrapperClientFactory mongoWrapperClientFactory = new MongoWrapperClientFactory() {
+    @Override public MongoClientWrapper createMongoClientWrapper( MongoProperties props, MongoUtilLogger log )
+      throws MongoDbException {
+      return MongoClientWrapperFactory.createMongoClientWrapper( props, log );
     }
+  };
+
+  protected static void setMongoWrapperClientFactory( MongoWrapperClientFactory mongoWrapperClientFactory ) {
+    MongoWrapperUtil.mongoWrapperClientFactory = mongoWrapperClientFactory;
+  }
+
+  protected static MongoWrapperClientFactory getMongoWrapperClientFactory() {
+    return mongoWrapperClientFactory;
+  }
+
+  public static MongoClientWrapper createMongoClientWrapper( MongoDbMeta mongoDbMeta, VariableSpace vars,
+                                                             LogChannelInterface log ) throws MongoDbException {
+    MongoProperties.Builder propertiesBuilder = new MongoProperties.Builder();
+    setIfNotNull( propertiesBuilder, MongoProp.HOST, vars.environmentSubstitute( mongoDbMeta.getHostnames() ) );
+    setIfNotNull( propertiesBuilder, MongoProp.PORT, vars.environmentSubstitute( mongoDbMeta.getPort() ) );
+    setIfNotNull( propertiesBuilder, MongoProp.DBNAME, vars.environmentSubstitute( mongoDbMeta.getDbName() ) );
+    setIfNotNull( propertiesBuilder, MongoProp.connectTimeout, mongoDbMeta.getConnectTimeout() );
+    setIfNotNull( propertiesBuilder, MongoProp.socketTimeout, mongoDbMeta.getSocketTimeout() );
+    setIfNotNull( propertiesBuilder, MongoProp.readPreference, mongoDbMeta.getReadPreference() );
+    setIfNotNull( propertiesBuilder, MongoProp.writeConcern, mongoDbMeta.getWriteConcern() );
+    setIfNotNull( propertiesBuilder, MongoProp.wTimeout, mongoDbMeta.getWTimeout() );
+    setIfNotNull( propertiesBuilder, MongoProp.JOURNALED, Boolean.toString( mongoDbMeta.getJournal() ) );
+    setIfNotNull( propertiesBuilder, MongoProp.USE_ALL_REPLICA_SET_MEMBERS,
+      Boolean.toString( mongoDbMeta.getUseAllReplicaSetMembers() ) );
+    setIfNotNull( propertiesBuilder, MongoProp.USERNAME, mongoDbMeta.getAuthenticationUser() );
+    setIfNotNull( propertiesBuilder, MongoProp.PASSWORD, mongoDbMeta.getAuthenticationPassword() );
+    setIfNotNull( propertiesBuilder, MongoProp.USE_KERBEROS,
+      Boolean.toString( mongoDbMeta.getUseKerberosAuthentication() ) );
+    if ( mongoDbMeta.getReadPrefTagSets() != null ) {
+      StringBuilder tagSet = new StringBuilder();
+      for ( String tag : mongoDbMeta.getReadPrefTagSets() ) {
+        tagSet.append( tag );
+        tagSet.append( "," );
+      }
+      // Remove trailing comma
+      if ( tagSet.length() > 0 ) {
+        tagSet.setLength( tagSet.length() - 1 );
+      }
+      setIfNotNull( propertiesBuilder, MongoProp.tagSet, tagSet.toString() );
+    }
+    return mongoWrapperClientFactory
+      .createMongoClientWrapper( propertiesBuilder.build(), new KettleMongoUtilLogger( log ) );
+  }
+
+  private static void setIfNotNull( MongoProperties.Builder builder, MongoProp prop, String value ) {
+    if ( value != null ) {
+      builder.set( prop, value );
+    }
+  }
 }
